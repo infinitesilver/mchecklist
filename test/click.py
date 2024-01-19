c9e5785221bc1d4703b282057b91ce95a703d81b
infinitesilver <jason.ribeiro4sc@gmail.com>
2024-01-19 00:43:15 -0500

    Test click.py

---
+++ test/click.py
@@ -0,0 +1,10 @@
+import click
+
+@click.command()
+@click.option('--path', help='path to destination')
+def path_create(path):
+    """Creates the designated path! (Not actually, just pretends to)"""
+    print(f"{path} created.")
+
+if __name__ == '__main__':
+    path_create()
