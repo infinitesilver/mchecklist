13012d50a5b8e82db7ae06dd06ab27665c7c3dd1
infinitesilver <jason.ribeiro4sc@gmail.com>
2024-01-19 01:10:02 -0500

    Rename click.py

--- test/click_test.py
+++ test/click_test.py
@@ -2,7 +2,7 @@ import click
 
 @click.command()
 @click.option('--path', help='path to destination')
-def path_create(path):
+def path_create(path: str):
     """Creates the designated path! (Not actually, just pretends to)"""
     print(f"{path} created.")
 
