87240083ba0328ca3920845f302c48041f67ee0b
infinitesilver <jason.ribeiro4sc@gmail.com>
2024-01-19 17:08:49 -0500

    Edit click_test.py

--- test/click_test.py
+++ test/click_test.py
@@ -1,10 +1,26 @@
 import click
+from click import echo
+
+@click.group()
+def cli():
+    pass
 
 @click.command()
 @click.option('--path', help='path to destination')
 def path_create(path: str):
     """Creates the designated path! (Not actually, just pretends to)"""
-    print(f"{path} created.")
+
+    echo(f"{path} created.")
+
+
+@click.command()
+def hello():
+    """Says hello"""
+
+    echo('hello')
+
 
 if __name__ == '__main__':
-    path_create()
+    cli.add_command(path_create)
+    cli.add_command(hello)
+    cli()
