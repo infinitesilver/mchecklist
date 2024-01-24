98e6b9394f75b872dd541122c590424d8647fd53
infinitesilver <jason.ribeiro4sc@gmail.com>
2024-01-24 14:42:40 -0500

    Start mchecklist.py

--- mchecklist/mchecklist.py
+++ mchecklist/mchecklist.py
@@ -1,2 +1,20 @@
 import json
+from pathlib import Path
 
+
+def _get_checklist_path(name: str) -> Path:
+    source_path = Path(__file__).resolve()
+    source_dir = source_path.parent
+    return Path(f"{source_dir}/checklists/{name}.json")
+
+def init_database(name="") -> None:
+    source_path = Path(__file__).resolve()
+    source_dir = source_path.parent
+    Path(f"{source_dir}/checklists").mkdir(exist_ok=True)
+    checklists_dir = Path(f"{source_dir}/checklists")
+    checklist = open(f"{checklists_dir}/{name}.json")
+    checklist.close()
+
+
+# Debug
+    init_database("checklist1")
